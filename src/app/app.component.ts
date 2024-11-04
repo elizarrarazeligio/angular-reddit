c570c42747c5a3d9a3c722bbe1f0332eb3821e24
Eligio Elizarraraz <eligio2008@live.com.mx>
2024-11-04 13:52:24 -0600

    feat: added button for user intercation by submitting information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
 }
